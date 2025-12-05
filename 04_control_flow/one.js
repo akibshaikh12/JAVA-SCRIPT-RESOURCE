9020e1c20d65223f5d11f45ce29a5397e6910ef3
akibshaikh12 <akibshaikh9709@gmail.com>
2025-12-05 19:37:48 +0530

    completed one.js and start switch

---
+++ 04_control_flow/one.js
@@ -0,0 +1,57 @@
+// if
+
+// const isUserLoggedIn = true;
+const temperature = 41;
+
+if (temperature == 40) {
+    // console.log("less than 50 ");
+    
+}else{
+    // console.log("temperature is greater than 50");
+};
+
+
+
+// < , > , <= , >= , == , === , != , !==
+
+
+// const score = 200;
+
+// if (score > 100) {
+//     const power = "fly"
+//     console.log(`User power: ${power}`);
+    
+// }
+
+const balance = 1000;
+
+// if (balance > 500) {
+//     console.log("test ");
+    
+// }
+
+// if (balance < 500) {
+//     console.log("less than 500");
+    
+// }else if (balance < 750) {
+//     console.log("less than 750");
+// }else if (balance < 900) {
+//     console.log("less than 900");
+// }else{
+//     console.log("less than 1200");
+// }
+
+const isUserLoggedIn = true;
+const debitCard = true;
+const loggedInfromGoogle = false;
+const loggedInfromEmail = true;
+
+if (isUserLoggedIn && debitCard && 2==3) {
+    console.log("Allow user to make a purchae");
+    
+}
+
+if (loggedInfromGoogle || loggedInfromEmail) {
+    console.log("User Logged In");
+    
+}
