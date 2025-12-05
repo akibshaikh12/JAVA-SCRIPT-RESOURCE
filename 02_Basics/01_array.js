b7f350d7e1d60700d74aa4079afc36d2e290b827
akibshaikh12 <akibshaikh9709@gmail.com>
2025-12-05 21:45:26 +0530

    completed two.js , three.js, four.js, five.js

--- 02_Basics/01_array.js
+++ 02_Basics/01_array.js
@@ -33,12 +33,12 @@ const newArr = myArr.join()
 
 // Slice , Splice
 
-console.log("A ", myArr);
+// console.log("A ", myArr);
 
 const myn1 = myArr.slice(1, 3)
 
-console.log(myn1);
-console.log("B ", myArr);
+// console.log(myn1);
+// console.log("B ", myArr);
 
 const myn2 = myArr.splice(1, 3)
 console.log("C ", myn1);
